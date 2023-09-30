adfe807bf78ecc675f759d924721878dc37906f8
Konstantin <kostyacherepanov1@gmail.com>
2023-09-30 13:27:52 +0700

    first commit

---
+++ cmd/shortener/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+var storage = make(map[string]string)
+var counter int
+
+func getMethod(res http.ResponseWriter, req *http.Request) {
+	for k, v := range storage {
+		if "/"+v == req.URL.String() {
+			res.Header().Set("Content-Type", "text/plain")
+			res.Header().Add("Location", k)
+			res.WriteHeader(307)
+			fmt.Println(res.Header())
+			res.Write([]byte(k))
+			return
+		}
+	}
+}
+
+func postMethod(res http.ResponseWriter, req *http.Request) {
+	counter++
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	storage[string(body)] = "shortenLink" + strconv.Itoa(counter)
+	shortenLink := "http://localhost:8080/" + storage[string(body)]
+	res.Header().Set("Content-Type", "text/plain")
+	res.WriteHeader(201)
+	res.Write([]byte(shortenLink))
+}
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		getMethod(res, req)
+	case http.MethodPost:
+		postMethod(res, req)
+	default:
+		http.Error(res, "Bad Request", http.StatusBadRequest)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", mainPage)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/knstch/shortener_url
+
+go 1.21.1
