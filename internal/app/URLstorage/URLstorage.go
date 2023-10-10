b6bf68a21678d224bf2d9aaa3cdc44c3e0376b5b
Konstantin <kostyacherepanov1@gmail.com>
2023-10-10 23:30:19 +0700

    fixed all issues

--- internal/app/URLstorage/URLstorage.go
+++ internal/app/URLstorage/URLstorage.go
@@ -5,9 +5,10 @@ import "sync"
 type Storage struct {
 	Data    map[string]string
 	Counter int
-	Mu      sync.Mutex
+	Mu      *sync.Mutex
 }
 
 var StorageURLs = Storage{
 	Data: make(map[string]string),
+	Mu:   &sync.Mutex{},
 }
