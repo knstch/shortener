7fa7169d1d39b16242ce36032d2873fb58cee146
Konstantin <kostyacherepanov1@gmail.com>
2023-10-29 22:14:59 +0700

    enabled tests

--- internal/app/getShortenLink/GetShortenLink_test.go
+++ internal/app/getShortenLink/GetShortenLink_test.go
@@ -1,40 +1,43 @@
 package getshortenlink
 
-// import (
-// 	"sync"
-// 	"testing"
+import (
+	"sync"
+	"testing"
 
-// 	URLstorage "github.com/knstch/shortener/internal/app/URLstorage"
-// 	"github.com/stretchr/testify/assert"
-// )
+	URLstorage "github.com/knstch/shortener/internal/app/URLstorage"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestGetShortenLink(t *testing.T) {
+func TestGetShortenLink(t *testing.T) {
 
-// 	var testStorage = URLstorage.Storage{
-// 		Data: make(map[string]string),
-// 		Mu:   &sync.Mutex{},
-// 	}
-// 	testStorage.Data["shortenLink1"] = "https://practicum.yandex.ru/"
-
-// 	tests := []struct {
-// 		name    string
-// 		want    string
-// 		request string
-// 	}{
-// 		{
-// 			name:    "First test",
-// 			want:    "https://practicum.yandex.ru/",
-// 			request: "shortenLink1",
-// 		},
-// 		{
-// 			name:    "Bad request test",
-// 			want:    "",
-// 			request: "asdwasd",
-// 		},
-// 	}
-// 	for i, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			assert.Equal(t, GetShortenLink((tests[i].request), testStorage), tests[i].want)
-// 		})
-// 	}
-// }
+	var testStorage = URLstorage.Storage{
+		Data: []URLstorage.Links{},
+		Mu:   &sync.Mutex{},
+	}
+	var data = URLstorage.Links{
+		ShortLink: "shortenLink1",
+		LongLink:  "https://practicum.yandex.ru/",
+	}
+	testStorage.Data = append(testStorage.Data, data)
+	tests := []struct {
+		name    string
+		want    string
+		request string
+	}{
+		{
+			name:    "First test",
+			want:    "https://practicum.yandex.ru/",
+			request: "shortenLink1",
+		},
+		{
+			name:    "Bad request test",
+			want:    "",
+			request: "asdwasd",
+		},
+	}
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, GetShortenLink((tests[i].request), testStorage), tests[i].want)
+		})
+	}
+}
