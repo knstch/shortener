39ace8ffc57230904deaaeb78a018d91ed8c0307
Konstantin <kostyacherepanov1@gmail.com>
2023-11-04 23:08:03 +0700

    added connection with DB

--- internal/app/DBConnect/DBConnect.go
+++ internal/app/DBConnect/DBConnect.go
@@ -20,7 +20,7 @@ func OpenConnection(host string, user string, password string, dbname string) er
 	}
 	defer db.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err = db.PingContext(ctx); err != nil {
